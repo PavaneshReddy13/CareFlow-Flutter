cfc5aaddff78db911aaf22eef9663a450e96503b
PavaneshReddy13 <PavaneshReddy13@users.noreply.github.com>
2026-06-23 07:32:53 +0000

    deploy: 399a8b49ed48bc68216e6fbf4c4083b462cd0ce9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "770377864" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1759645147" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
